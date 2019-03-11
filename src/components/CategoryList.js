5dbe0d6e95320d33f4cbfeb9f0af11f2fd2a1647
Sergey Tsivin <sergey.tsivin@gmail.com>
2019-03-11 21:43:00 +0300

    Populates with DocumentReference: code cleanup

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -8,16 +8,11 @@ import AddCategory from './AddCategory'
 
 
 class CategoryList extends Component {
-    // static propTypes = {
-    //     uid: PropTypes.string,
-    //     categories: PropTypes.arrayOf(PropTypes.string)
-    // };
 
-
-    renderCategory(category) {
+    static renderCategory(category) {
         return <div key={category.name}>
             <b>{category.name}</b>
-            (created by: {category.user.displayName})
+            (created by: {category.userData.displayName})
         </div>
     }
 
@@ -32,7 +27,7 @@ class CategoryList extends Component {
         console.log('categories', categories);
 
         const categoryItems = Object.values(categories).map(
-            (category) => this.renderCategory(category)
+            (category) => CategoryList.renderCategory(category)
         );
 
         return (
@@ -50,27 +45,18 @@ const collection = 'categories';
 
 
 const populates = (dataKey, originalData) => {
-    console.log('populates', dataKey, originalData);
-    // debugger;
-    if (dataKey === 'categories') {
-        Object.keys(originalData).forEach((key) => { originalData[key].user = originalData[key].userRef.path.split('/')[1] })
-    }
-    else if (dataKey) {
-        originalData['user'] = originalData.userRef.path.split('/')[1];
-    }
-    return [{child: 'user', root: 'users'}]
-};
 
-// const populates = [
-//     {child: 'user', root: 'users'} // replace uid with user object
-//     // {child: 'userRef', root: 'users'} // replace uid with user object
-// ];
+   if (originalData && originalData['userRef'] && !originalData['userData']) {
+        originalData['userData'] = originalData.userRef.path.split('/')[1];
+    }
+
+    return [{child: 'userData', root: 'users'}]
+};
 
 const mapStateToProps = state => {
     return {
         uid: state.firebase.auth.uid,
         categories: populate(state.firestore, collection, populates)
-        // categories: state.firestore.ordered.categories ? state.firestore.ordered.categories.map(c => c.name) : [],
     }
 };
 
